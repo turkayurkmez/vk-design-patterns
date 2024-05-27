493e7babaf3610a921b6501375e1cd9a8e5b6b82
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-27 13:49:22 +0300

    Encapsulation in objects

---
+++ OOPOverview/Encapsulation/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Product
+    {
+        private decimal price;
+
+        public void SetPrice(decimal value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Fiyat negatif olamaz");
+
+            }
+
+            this.price = value;
+        }
+
+        public decimal GetPrice() { return price; }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new ArgumentNullException();
+                }
+                name = value;
+            }
+        }
+
+        public double Rating { get; set; }
+
+        public decimal DiscountedPrice { get;  }
+        public Product()
+        {
+            DiscountedPrice = price * 0.95m;
+        }
+
+        public int StockCount { get; private set; }
+        public void UpdateStock()
+        {
+            this.StockCount += 100;
+        }
+    }
+}
---
+++ OOPOverview/Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Encapsulation;
+
+Product product = new Product();
+
+
+decimal value = -5;
+if (value<0)
+{
+    //exception fırlat
+}
+
+//product.Price = value;
+product.SetPrice(-16);
+Console.WriteLine(product.Rating);
+product.Rating = 4.6;
+
+
