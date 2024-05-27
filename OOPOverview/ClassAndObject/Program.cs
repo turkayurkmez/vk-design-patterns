71b299822d648062f9bdb7ac3b52a59dbe7f6c8a
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-27 12:11:40 +0300

    Explained class and object

---
+++ OOPOverview/ClassAndObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassAndObject;
+
+Console.WriteLine("Test");
+
+Product hat = new Product();
+Product keyboard = new Product();
+
+hat.Name = "Indiana Jones Şapkası";
+
+Product sunGlass = hat;
+sunGlass.Name = "Robocop sunglasses";
+Console.WriteLine(hat.Name);
+//sunglass Heap adresi ile -> hat adresi aynı
+
+
