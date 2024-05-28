742f7b0ad16d0f9184afde021dafeae0a39cdf82
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-28 11:09:41 +0300

    Open Closed explained

---
+++ SOLID/OpenClosedPrinciple/BookingManagement.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+
+    //public enum CountryInfo
+    //{
+    //    Germany,
+    //    Sweden,
+    //    Turkiye,
+    //    Italy
+    //}
+
+    public abstract class CountryInfo
+    {
+        public abstract void CreateOnDb();       
+    }
+
+    public class Turkiye : CountryInfo
+    {
+        public override void CreateOnDb()
+        {
+            Console.WriteLine("Türkiye db'si ile çalışıldı");
+        }
+    }
+
+    public class Sweden : CountryInfo
+    {
+        public override void CreateOnDb()
+        {
+            Console.WriteLine("İsveç db'si ile çalışıldı");
+        }
+    }
+
+    public class Germany : CountryInfo
+    {
+        public override void CreateOnDb()
+        {
+            Console.WriteLine("Almanya db'si ile çalışıldı");
+        }
+    }
+
+    public class Italy : CountryInfo
+    {
+        public override void CreateOnDb()
+        {
+            Console.WriteLine("İtalya db'si ile çalışıldı");
+        }
+    }
+
+
+    public class BookingDetails
+    {
+        public string Owner { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public CountryInfo CountryInfo { get; set; }
+
+    }
+    public class BookingManagement
+    {
+        public BookingDetails BookingDetails { get; set; }
+        public void CreateBookingOnDb()
+        {
+            //switch (BookingDetails.CountryInfo)
+            //{
+            //    case CountryInfo.Germany:
+            //        Console.WriteLine("Almanya db'si ile çalışıldı");
+            //        break;
+            //    case CountryInfo.Sweden:
+            //        Console.WriteLine("İsveç db'si ile çalışıldı");
+
+            //        break;
+            //    case CountryInfo.Turkiye:
+            //        Console.WriteLine("Türkiye db'si ile çalışıldı");                      
+
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            BookingDetails.CountryInfo.CreateOnDb();
+        }
+    }
+}
---
+++ SOLID/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenClosedPrinciple;
+
+Console.WriteLine("Hello, World!");
+/*
+ *  OCP:
+ *  Bir nesne .........Gelişime......... AÇIK .........Değişime....... KAPALI olmalıdır.
+ *    
+ *    --Yeni Özellik eklemek....
+ *   Varolan ve çalışan bir uygulamaya yeni bir özellik eklemek istediğinizde eski nesnelere müdahale etmeniz gerekiyorsa, prensibi ihlal ediyorsunuz demektir.
+ */
+
+BookingDetails bookingDetails = new BookingDetails() { Owner="Türkay", CountryInfo= new Italy(), CheckInDate=DateTime.Now};
+BookingManagement bookingManagement = new BookingManagement() {  BookingDetails = bookingDetails};
+bookingManagement.CreateBookingOnDb();
