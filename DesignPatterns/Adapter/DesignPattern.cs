eb328e865a548029d414fbd657c756ec7e9ff0ac
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-29 14:36:00 +0300

    Adapter patten explained

---
+++ DesignPatterns/Adapter/DesignPattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    /*
+     *  Üçüncü parti bir bileşen geriye XML döndürüyor.
+     *  Ancak bizim JSON (veya object) türünde veriye ihtiyacımız var
+     *  
+     */
+
+    public class Product
+    {
+
+    }
+
+    //Client Interface: istemcinin doğrudan kullanabileceği nesne
+    public interface IDataTransfer
+    {
+        public List<Product> GetProducts();
+    }
+
+    //Önceden yazılmış harici bileşende erişmek istediğiniz fonksiyonu barındıran kaynak:
+    public class XMLSource : IDataTransfer
+    {
+        public List<Product> GetProducts()
+        {
+            Console.WriteLine("XML'den veriyi aldı");
+            return new List<Product>();
+        }
+    }
+
+
+
+    //3. Adaptör: İhtiyacınız olan türe dönüştürecek nesne
+
+    public class JsonAdapter : IDataTransfer
+    {
+        private readonly XMLSource source;
+
+        public JsonAdapter(XMLSource source)
+        {
+            this.source = source;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return source.GetProducts();
+        }
+    }
+
+
+}
---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Adapter;
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Uygulamanızda bir kaynaktan gelen belirli bir formatta veriye ihtiyacınız var. Fakat kaynağın ürettiği veri formatı ihtiyacınız olandan farklı. 
+ */
+
+XMLSource source = new XMLSource();
+JsonAdapter jsonAdapter = new JsonAdapter(source);
+jsonAdapter.GetProducts();
+
+SqlDataAdapter adapter = new SqlDataAdapter("", ""); //db'deki veriyi -> uygulamada dataTable nesnesine dönüştür.
+DataTable dataTable = new DataTable();
+adapter.Fill(dataTable);
